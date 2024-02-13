fc862c9421e0322800ec14cd81b20472fd1f6343
naldi <syafrinaldi@eunoia.asia>
2024-02-13 22:16:46 +0700

    another commit

--- app/components/Banners.tsx
+++ app/components/Banners.tsx
@@ -10,46 +10,49 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 
 function Banners() {
-    const [banners] = useMainStore((state) => [state.banners]);
-    const [isMobile, setIsMobile] = useState(false)
+  const [banners] = useMainStore((state) => [state.banners]);
+  const [isMobile, setIsMobile] = useState(false);
+  const isBrowser = () => typeof window !== "undefined";
 
-    const onResize = () => {
-        const width = window.innerWidth
-        setIsMobile(width <= 767)
-    }
-    useEffect(() => {
-        onResize();
-        window.addEventListener("resize", onResize);
-    }, [])
-    return (
-      <Swiper
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation, Pagination, Autoplay]}
-        centeredSlides={true}
-        spaceBetween={30}
-        slidesPerView={1}
-        loop={true}
-        autoplay={{ delay: 2000 }}
-        className="w-full banners"
-      >
-        {banners.map((item) => {
-          return (
-            <SwiperSlide
-              className="w-full flex flex-col relative"
-              key={item.mainBannerId}
-            >
-              <img
-                src={isMobile ? item.mobileImageUrl : item.pcImageUrl}
-                alt={item.title}
-              />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    );
+  const onResize = () => {
+    if (!isBrowser()) return;
+    const width = window.innerWidth;
+    setIsMobile(width <= 767);
+  };
+  useEffect(() => {
+    onResize();
+    if (!isBrowser()) return;
+    return window.addEventListener("resize", onResize);
+  }, []);
+  return (
+    <Swiper
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Navigation, Pagination, Autoplay]}
+      centeredSlides={true}
+      spaceBetween={30}
+      slidesPerView={1}
+      loop={true}
+      autoplay={{ delay: 2000 }}
+      className="w-full banners"
+    >
+      {banners.map((item) => {
+        return (
+          <SwiperSlide
+            className="w-full flex flex-col relative"
+            key={item.mainBannerId}
+          >
+            <img
+              src={isMobile ? item.mobileImageUrl : item.pcImageUrl}
+              alt={item.title}
+            />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 }
 
 export default Banners;
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["dvd6ljcj7w3pj.cloudfront.net"],
+    domains: [
+      "dvd6ljcj7w3pj.cloudfront.net",
+      "prod-testvalley.s3.ap-northeast-2.amazonaws.com",
+    ],
   },
 };
 
